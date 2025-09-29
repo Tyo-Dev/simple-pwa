62e3be3d170ab48f2ad54836df8114473b590bc2
Catur Setyono <catursetyono542@gmail.com>
2025-09-29 20:04:36 +0700

    rm tailwind cdn

--- sw.js
+++ sw.js
@@ -1,30 +1,45 @@
-const CACHE_NAME = 'ttt-cache-v1';
+const CACHE_NAME = "ttt-cache-v1";
 const ASSETS = [
-'/',
-'/index.html',
-'/manifest.json',
-'https://cdn.tailwindcss.com'
+  "/",
+  "/index.html",
+  "/manifest.json",
 ];
 
-
-self.addEventListener('install', (event) => {
-event.waitUntil(
-caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS)).then(() => self.skipWaiting())
-);
+self.addEventListener("install", (event) => {
+  event.waitUntil(
+    caches
+      .open(CACHE_NAME)
+      .then((cache) => cache.addAll(ASSETS))
+      .then(() => self.skipWaiting())
+  );
 });
 
-
-self.addEventListener('activate', (event) => {
-event.waitUntil(
-caches.keys().then((keys) => Promise.all(keys.map(k => { if(k !== CACHE_NAME) return caches.delete(k); }))).then(() => self.clients.claim())
-);
+self.addEventListener("activate", (event) => {
+  event.waitUntil(
+    caches
+      .keys()
+      .then((keys) =>
+        Promise.all(
+          keys.map((k) => {
+            if (k !== CACHE_NAME) return caches.delete(k);
+          })
+        )
+      )
+      .then(() => self.clients.claim())
+  );
 });
 
-
-self.addEventListener('fetch', (event) => {
-event.respondWith(
-caches.match(event.request).then((cached) => cached || fetch(event.request).then(res => {
-return res;
-})).catch(() => caches.match('/index.html'))
-);
+self.addEventListener("fetch", (event) => {
+  event.respondWith(
+    caches
+      .match(event.request)
+      .then(
+        (cached) =>
+          cached ||
+          fetch(event.request).then((res) => {
+            return res;
+          })
+      )
+      .catch(() => caches.match("/index.html"))
+  );
 });
